3d50544bdb3ef881000f275aecfe3deab798a0bd
Vincent Fuseau <vFiouze@MacBook-Pro-de-Vincent.local>
2020-05-06 12:59:02 -0700

    add urls

--- bibliothequesite/urls.py
+++ bibliothequesite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^bibliotheque/',include('bibliothequeapp.urls')),
+    url(r'^test/',include('bibliothequeapp.urls')),
 ]
